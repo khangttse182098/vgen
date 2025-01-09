3c7274c26ef40d868db5da5891c3d09f3b37ab5b
minhldse181522@fpt.edu.vn <minhldse181522@fpt.edu.vn>
2025-01-09 10:00:51 +0700

    update

--- src/app/bai-viet/page.tsx
+++ src/app/bai-viet/page.tsx
@@ -1,4 +1,4 @@
-import Post from "@/components/normalPost";
+import Post from "@/app/bai-viet/components/normalPost";
 import React from "react";
 import style from "./index.module.scss";
 import HightlightPost from "./components/highlightPost/HightlightPost";
