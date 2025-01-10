647c1e3273a1785abe27c298b50652974112e33b
khanghnse181562@fpt.edu.vn <khanghnse181562@fpt.edu.vn>
2025-01-10 14:32:57 +0700

    feat: add topic list, popular author list and highlight event

--- src/app/bai-viet/page.tsx
+++ src/app/bai-viet/page.tsx
@@ -2,12 +2,18 @@ import Post from "@/app/bai-viet/components/normalPost";
 import React from "react";
 import style from "./index.module.scss";
 import HightlightPost from "./components/highlightPost/HightlightPost";
+import TopicList from "./components/topicList/TopicList";
+import PopularAuthorList from "./components/popularAuthorList/PopularAuthorList";
+import HighlightEvent from "./components/highlightEvent/HighlightEvent";
 
 const PostPage = () => {
   return (
     <div>
       <HightlightPost />
       <Post />
+      <TopicList />
+      <HighlightEvent />
+      <PopularAuthorList />
     </div>
   );
 };
