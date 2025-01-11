7ef0b66e97f988cedad9e4006d53183d8867de3c
minhldse181522@fpt.edu.vn <minhldse181522@fpt.edu.vn>
2025-01-11 17:14:12 +0700

    fix layout

--- src/app/bai-viet/components/topicOption/TopicOption.tsx
+++ src/app/bai-viet/components/topicOption/TopicOption.tsx
@@ -5,21 +5,21 @@ import Image, { StaticImageData } from "next/image";
 import { IconType } from "react-icons/lib";
 
 type TTopicOption = {
-    Icon: IconType,
-    name: string
-}
+  Icon: IconType;
+  name: string;
+};
 
-const TopicOption = ({Icon, name}: TTopicOption) => {
-    return (
-        <>
-            <div className={`${style["option"]}`}>
-                <span className={`${style["circle"]}`}>
-                    <Icon />
-                </span>
-                <span className={`${style["text"]}`}>{name}</span>
-            </div>
-        </>
-    )
-}
+const TopicOption = ({ Icon, name }: TTopicOption) => {
+  return (
+    <>
+      <div className={`${style["container"]}`}>
+        <span className={`${style["container__circle"]}`}>
+          <Icon />
+        </span>
+        <span className={`${style["container__text"]}`}>{name}</span>
+      </div>
+    </>
+  );
+};
 
 export default TopicOption;
