aa1af0b1929c3a7e07f4c10c06a9a65a3a366262
minhldse181522@fpt.edu.vn <minhldse181522@fpt.edu.vn>
2025-01-11 18:17:56 +0700

    feat: du an giao duc

--- src/app/du-an-giao-duc/page.tsx
+++ src/app/du-an-giao-duc/page.tsx
@@ -1,7 +1,10 @@
+import Banner from "./components/banner/Banner";
+
 const EduPlanPage = () => {
   return (
     <div>
-      <h2>Các dự án giáo dục</h2>
+      <h2 style={{ marginLeft: "27px", marginTop: "30px" }}>Các dự án giáo dục</h2>
+      <Banner />
     </div>
   );
 };
